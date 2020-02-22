49b047b79b8169b1961508ab631827d2a23dd226
wdhg <wdhgrant@gmail.com>
2020-02-22 11:35:01 +0000

    add basic file

---
+++ src/Main.hs
@@ -0,0 +1,56 @@
+conversionMap :: [(String, String)]
+conversionMap
+  = [ ("\\A", "\\forall")
+    , ("\\E", " \\exists ")
+    , (" in ",  " \\in ")
+    , (" -> ", " \\rightarrow ")
+    , (" <-> ", " \\leftrightarrow ")
+    ]
+
+isPrefix :: String -> String -> Bool
+isPrefix "" _
+  = True
+isPrefix (_ : _) ""
+  = False
+isPrefix (x : xs) (y : ys)
+  = x == y && isPrefix xs ys
+
+replace :: String -> (String, String) -> String
+replace "" _
+  = ""
+replace text@(c : remaining) mapping@(prefix, replacement)
+  | isPrefix prefix text = replacement ++ text'
+  | otherwise            = c : replace remaining mapping
+    where
+      text'
+        = drop (length prefix) text
+
+embedFile :: String -> String
+embedFile text
+  = fileFront ++ text ++ fileEnd
+    where
+      fileFront
+        = "\\documentclass{article}\n\
+          \\\usepackage{dsfont}\n\
+          \\\usepackage{listings}\n\
+          \\\begin{document}\n"
+      fileEnd
+        = "\\end{document}"
+
+embedEqnt :: String -> String
+embedEqnt text
+  = eqntFront ++ text ++ eqntEnd
+    where
+      eqntFront
+        = "\\begin{equation}\n"
+      eqntEnd
+        = "\n\\end{equation}"
+
+convert :: String -> String
+convert text
+  = embedFile $ unlines $ map embedEqnt $ lines $ foldl replace text conversionMap
+
+main :: IO ()
+main = do
+  contents <- getContents
+  putStrLn $ convert contents
