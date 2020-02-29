1728055bc608d9d0a08395b82cc285312078b06a
wdhg <wdhgrant@gmail.com>
2020-02-29 09:51:24 +0000

    change AST data type to match what is actually needed

--- src/LogiTeX/Parser.hs
+++ src/LogiTeX/Parser.hs
@@ -3,23 +3,9 @@ module Parser where
 import Lexer
 
 data AST
-  = Node Token [Tree]
-  | Leaf Token
+  = Question [AST]
+  | Text token
 
-tokenBinding :: Token -> Int
-tokenBinding (Token sectionType _)
-  = binding sectionType
-
--- the lower the value the more binding
-binding :: SectionType -> Int
-binding EquationMulti
-  = 1
-binding Snippet
-  = 1
-binding LatexMulti
-  = 1
-binding _
-  = 0
 
 parse :: [Token] -> AST
 parse
