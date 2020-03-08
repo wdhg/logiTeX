0c9e82b2a932dfea42b82fd3d1e4f9301d9f5d12
wdhg <wdhgrant@gmail.com>
2020-03-08 12:53:27 +0000

    add basic compiler

--- src/LogiTeX/Compiler.hs
+++ src/LogiTeX/Compiler.hs
@@ -1,5 +1,142 @@
 module Compiler where
 
+import Lexer
+import Parser
+
+header :: String
+header
+  = "\\documentclass{article}\n\
+    \\\usepackage{dsfont, amssymb, amsmath}\n\
+    \\\usepackage{listings}\n\
+    \\\begin{document}\n\
+    \\\begin{flushleft}\n\
+    \\\begin{enumerate}\n"
+
+footer :: String
+footer
+  = "\\end{enumerate}\n\
+    \\\end{flushleft}\n\
+    \\\end{document}"
+
+(==>) :: a -> b -> (a, b)
+(==>)
+  = (,)
+
+wrappings :: [(SectionType, (String, String))]
+wrappings
+  = [ Comment ==>
+      ( ""
+      , ""
+      )
+    , Title ==>
+      ( "\\title{"
+      , "}\n\\date{}\n"
+      )
+    , Question ==>
+      ( "\\item\\begin{enumerate}\n"
+      , "\\end{enumerate}\n"
+      )
+    , SubQuestion ==>
+      ( "\\item\\begin{enumerate}\n"
+      , "\\end{enumerate}\n"
+      )
+    , SubSubQuestion ==>
+      ( "\\item\n"
+      , "\n"
+      )
+    , Equation ==>
+      ( "\\begin{equation}\n"
+      , "\\end{equation}\n"
+      )
+    , EquationMulti ==>
+      ( "\\begin{align}\\begin{split}\n"
+      , "\n\\end{split}\\end{align}\n"
+      )
+    , Snippet ==>
+      ( "\\begin{lstlisting}\n"
+      , "\n\\end{lstlisting}\n"
+      )
+    , Latex ==>
+      ( ""
+      , "\n"
+      )
+    , LatexMulti ==>
+      ( ""
+      , "\n"
+      )
+    , Text ==>
+      ( ""
+      , "\n"
+      )
+    ]
+
+type Mapping
+  = ([String], String)
+
+type Mappings
+  = [Mapping]
+
+globalMappings :: Mappings
+globalMappings
+  = [ ["=>"]  ==> " \\implies"
+    , ["<=>"] ==> "\\iff"
+    , ["/="]  ==> "\\neq"
+    , ["~="]  ==> "\\approx"
+    , [":="]  ==> "\\triangleq"
+    , ["<="]  ==> "\\leqslant"
+    , [">="]  ==> "\\geqslant"
+    ]
+
+logicMappings :: Mappings
+logicMappings
+  = [ ["%"] ==> ""
+    , ["\\A", "forall"] ==> "\\forall"
+    , ["\\E", "exists"] ==> "\\exists"
+    , ["in"] ==>  "\\in"
+    , ["&&", "and"] ==> "\\land"
+    , ["||", "or"] ==> "\\lor"
+    , ["not"] ==> "\\neg"
+    , ["precedes"] ==> "\\prec"
+    ]
+
+getMappings :: SectionType -> [Mappings]
+getMappings sectionType
+  | sectionType `elem` logicSections = [logicMappings, globalMappings]
+  | otherwise                        = [globalMappings]
+    where
+      logicSections
+        = [Equation, EquationMulti, Latex, LatexMulti]
+
+replace :: Mappings -> String -> String
+replace mappings text
+  = unwords $ map (replace' mappings) $ words text
+    where
+      replace' :: Mappings -> String -> String
+      replace' [] word
+        = word
+      replace' ((keywords, replacement) : mappings') word
+        | word `elem` keywords = replacement
+        | otherwise            = replace' mappings' word
+
+tokenToText :: Token -> String
+tokenToText (Token Comment _)
+  = ""
+tokenToText (Token sectionType text)
+  = foldr replace text $ getMappings sectionType
+
+wrap :: SectionType -> String -> String
+wrap sectionType text
+  = case lookup sectionType wrappings of
+      Nothing -> error $ "cannot find wrapping for " ++ show sectionType
+      Just (before, after) -> before ++ text ++ after
+
+-- convert strings
+-- wrap strings in LaTeX
+-- concat strings in order
 compile :: AST -> String
-compile
-  = undefined
+compile (Leaf token@(Token sectionType text))
+  = wrap sectionType $ tokenToText token
+compile (Node token@(Token sectionType _) trees)
+  = wrap sectionType $ concatMap compile $ reverse trees
+compile (Root trees)
+  = header ++ (concatMap compile $ reverse trees) ++ footer
